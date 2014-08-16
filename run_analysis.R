39c8bda49c362c50155b7487dae314889588fdaf
cjhuxley <cjhuxley@exemail.com.au>
2014-08-16 18:26:42 +1000

    run_analysis.R first commit
    Initial code merging most of the training and data into a "dataSet"
    frame for subsequent processing

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+#To be run_analysis.R
+#Download files
+# Merges the training and the test sets to create one data set.Includes addition of a column to indicate 
+# if it is test or train data.
+#
+setwd("C:/Users/Chris/Desktop/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
+setwd("train")
+trainData<-read.table("X_train.txt")
+setwd("C:/Users/Chris/Desktop/Getting and Cleaning Data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
+setwd("test")
+testData<-read.table("X_test.txt")
+subject<-read.table("subject_test.txt") # test subject labels
+testOrTrain<-rep("test",2947)#test lables for testOrTrain column
+#add activity lables
+testData2<-cbind(testOrTrain, subject, testData)
+testOrTrain<-rep("train",7352) #train lables for testOrTrain column
+subject<-read.table("subject_train.txt") # train subject labels
+trainData2<-cbind(testOrTrain,subject,trainData)
+#add activity lables
+# Merge training and test data into "dataSet"
+dataSet<-rbind(testData2,trainData2)
+
+# Add titles to trainData and add subject id 
+  #Deal with test labels as they are already open
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names. 
+#Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
